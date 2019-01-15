117a888563110dafd4eb9cb41c01efec4323ee68
PradalCyril <pradal.cyril@gmail.com>
2019-01-15 13:41:36 +0100

    correction de bug

--- generatorTuto/routes/index.js
+++ generatorTuto/routes/index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: 'tmp/', 
-limits:{
-fileSize : 3 * 1024 *1024,
-}
+const upload = multer({
+  dest: 'tmp/',
+  limits: {
+    fileSize: 3 * 1024 * 1024,
+  }
 });
 const fs = require('fs');
 
-router.post('/monupload', upload.array('monfichier',3), function (req, res, next) {
-  for(let i=0;i<3;i++){
-  fs.rename(req.files[i].path, 'public/images/' + req.files[i].originalname, function(err){
-    if (err) {
+router.post('/monupload', upload.array('monfichier', 3), function (req, res, next) {
+  for (let i = 0; i < req.files.length; i++) {
+    fs.rename(req.files[i].path, 'public/images/' + req.files[i].originalname, function (err) {
+      if (err) {
         res.send('problème durant le déplacement');
-    } else {
-        res.send('Fichier uploadé avec succès');
-    }
-  });
-}res.end()})
+      }
+    });
+  }
+    res.send('Fichier uploadé avec succès');
+})
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Expresssss' });
 });
 
